fcfd56a90aa25f73575825a4c06ef3cb62f6feec
Evan Howard <evanhoward@college.harvard.edu>
2023-05-05 20:15:08 -0400

    initial visualization

--- app.js
+++ app.js
@@ -47,6 +47,102 @@ function updateVisualization(element) {
     var currentScrolly = d3.select(element.parentNode.parentNode);
     var currentVisualization = currentScrolly.select(".visualization");
     currentVisualization.text("Visualization for step " + element.id);
+
+    if(element.id == "1") {
+        d3.csv("datasets/climate-change_yem.csv").then(data => {
+            const nestedData = d3.group(data, d => d["Indicator Name"]);
+          
+            // Task 2: Create line charts for each group
+            const charts = Array.from(nestedData, ([key, values]) => {
+              return {
+                indicatorName: key,
+                chartData: values.map(d => ({
+                  year: +d.Year,
+                  value: +d.Value
+                })).sort((a, b) => a.year - b.year)
+              };
+            });
+          
+            // Task 3: Arrange the line charts in a grid
+            const grid = currentVisualization.append("div")
+              .attr("class", "grid");
+          
+            const gridSize = 200;
+            const margin = {top: 20, right: 20, bottom: 30, left: 50};
+            const width = gridSize - margin.left - margin.right;
+            const height = gridSize - margin.top - margin.bottom;
+          
+            const x = d3.scaleLinear().range([0, width]);
+            const y = d3.scaleLinear().range([height, 0]);
+          
+            charts.forEach(chart => {
+              const div = grid.append("div")
+                .attr("class", "chart-container")
+                .style("width", `${gridSize}px`)
+                .style("height", `${gridSize}px`);
+          
+              const svg = div.append("svg")
+                .attr("width", gridSize)
+                .attr("height", gridSize)
+                .append("g")
+                .attr("transform", `translate(${margin.left}, ${margin.top})`);
+          
+              x.domain(d3.extent(chart.chartData, d => d.year));
+              y.domain([0, d3.max(chart.chartData, d => d.value)]);
+          
+              const line = d3.line()
+                .x(d => x(d.year))
+                .y(d => y(d.value));
+          
+              svg.append("path")
+                .datum(chart.chartData)
+                .attr("fill", "none")
+                .attr("stroke", "steelblue")
+                .attr("stroke-width", 1.5)
+                .attr("d", line);
+          
+              svg.append("g")
+                .attr("transform", `translate(0, ${height})`)
+                .call(d3.axisBottom(x).ticks(5));
+          
+              svg.append("g")
+                .call(d3.axisLeft(y).ticks(5));
+            });
+          
+            // Task 4: Implement click events to zoom in and out of the charts
+            container.selectAll(".chart-container")
+              .on("click", function() {
+                d3.select(this)
+                  .classed("zoomed", d => !d3.select(this).classed("zoomed"));
+              });
+          
+            container.append("style")
+              .html(`
+                .grid {
+                  display: flex;
+                  flex-wrap: wrap;
+                  gap: 10px;
+                }
+                .chart-container {
+                  position: relative;
+                  cursor: pointer;
+                }
+                .chart-container.zoomed {
+                    position: absolute;
+                    top: 0;
+                    left: 0;
+                    width: 100%;
+                    height: 100%;
+                    z-index: 100;
+                  }
+                  .chart-container.zoomed svg {
+                    width: 100%;
+                    height: 100%;
+                  }
+              `);
+          
+          });
+    }
 }
 
 function init() {
